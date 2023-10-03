b7a9ed94fe592dbe89363fa101b5a19961254e49
Jeremy <jdavid.rupprecht@gmail.com>
2023-10-03 17:55:55 -0600

    Create basic operator functions

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("testing...");
+function add(a,b) {
+    return a + b
+}
+
+function subtract(a,b) {
+    return a - b
+}
+
+function multiply(a,b) {
+    return a * b
+}
+
+function divide(a,b) {
+    return a / b
+}
