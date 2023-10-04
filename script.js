ed6c3c0513612a96aa036654859172b837fb8ba7
Jeremy <jdavid.rupprecht@gmail.com>
2023-10-03 22:03:50 -0600

    Add basic functionality to display and clear digits

--- script.js
+++ script.js
@@ -27,16 +27,29 @@ function operate(op1, operand, op2) {
     }
 }
 
-let expression = "";
-let op1 = 1;
+function saveAndDisplayDigit(digit) {
+    // Digits replace zero displayed by itself, otherwise concatenate them 
+    // to build numbers
+    if (+op1 === 0) {
+        op1 = digit;
+    } else {
+        op1 += digit;
+    }
+    digitDisplay.textContent = +op1;
+    // put logic for putting a number into op2 here
+    
+}
+
+let op1 = "0";
 let operand = "+";
-let op2 = 1;
-//console.log(operate(op1, operand, op2));
+let op2 = "0";
+
+const digitDisplay = document.getElementById("digitDisplay");
 
 let digitButtons = document.querySelectorAll(".digit");
 digitButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        console.log(button);
+        saveAndDisplayDigit(button.id);
     });
 });
 
@@ -48,6 +61,11 @@ operatorButtons.forEach((button) => {
 });
 
 const clear = document.getElementById("C");
+clear.addEventListener('click', () => {
+    op1 = "0";
+    op2 = "0";
+    digitDisplay.textContent = "0";
+});
 const backspace = document.getElementById("B");
 const switchSign = document.getElementById("+/-");
 const dot = document.getElementById(".");
