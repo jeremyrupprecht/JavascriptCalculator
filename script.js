575a017d9f6762a7ccf5df24ea9f3288c219f93a
Jeremy <jdavid.rupprecht@gmail.com>
2023-10-03 18:01:54 -0600

    Create basic operate function with only 2 operator +,-,*,/ functionality

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ function multiply(a,b) {
 function divide(a,b) {
     return a / b
 }
+
+function operate(op1, operand, op2) {
+    switch(operand) {
+        case "+":
+            return add(op1, op2)
+        case "-":
+            return subtract(op1, op2)
+        case "*":
+            return multiply(op1, op2)
+        case "/":
+            return divide(op1, op2)
+    }
+}
+
+let expression = "";
+let op1 = 1;
+let operand = "+";
+let op2 = 1;
+console.log(operate(op1, operand, op2));
+
+
+
+
