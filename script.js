8dc5c45b98e6ee346fd0730a49a34288a4c61ce3
Jeremy <jdavid.rupprecht@gmail.com>
2023-10-09 04:55:41 -0600

    Implement keyboard based eventlisteners

--- script.js
+++ script.js
@@ -61,14 +61,6 @@ function saveAndDisplayDot() {
     digitDisplay.textContent = operand;
 }
 
-function divideByZero() {
-    op1 = "0";
-    operand = "+";
-    op2 = "0";
-    firstDigit = true;
-    workingWithOp2 = false;
-    digitDisplay.textContent = "Can't Do That";
-}
 
 function calculateAndDisplayResult() {
     if (operand === "/"  && op2 === "0") {
@@ -76,20 +68,17 @@ function calculateAndDisplayResult() {
         return 
     }
     result = operate(+op1, operand, +op2);
-    /* 
-    2 cases at this point:
+    /* 2 cases at this point:
         1. The user wants to make a completely new calculation --> press any digit
                 -firstDigit and workingWithOp2 flags are reset, meaning:
                     -Pressing any digit clears this result
                     -Any new digits are saved into the cleared op1
         OR
-
         2. The user wants to use this result --> press any operator
                 -This result is saved in op1
                 -firstDigit and workingWithOp2 flags are reset BUT pressing 
                  any operator will set workingWithOp2 back to true so that 
-                 new input is saved into op2, with this result in op1
-    */
+                 new input is saved into op2, with this result in op1 */
     op1 = result.toString();
     firstDigit = true;
     workingWithOp2 = false;
@@ -97,12 +86,26 @@ function calculateAndDisplayResult() {
     digitDisplay.textContent = result;
 }
 
+function changeOperator(operandToChangeTo) {
+    operand = operandToChangeTo;
+    workingWithOp2 = true;
+    firstDigit = true;
+}
+
+function clearAllData() {
+    op1 = "0";
+    op2 = "0";
+    operand = "+";
+    workingWithOp2 = false;
+    firstDigit = true;
+    digitDisplay.textContent = "0";
+}
+
 function backSpaceDigit() {
     let operand = workingWithOp2 ? op2 : op1;
     if (operand.length > 1) {
         // Remove last/rightmost character from the string
         operand = operand.slice(0, -1);
-        console.log("SLICE: " + operand);
     } else {
         // Not touching workingWithOp2 here, either operand can be
         // backspaced to 0 without affecting anything else
@@ -128,6 +131,15 @@ function switchSign() {
     }
     operand = roundNumbersWithLongDecimals(operand);
     digitDisplay.textContent = operand;
+}
+
+function divideByZero() {
+    op1 = "0";
+    operand = "+";
+    op2 = "0";
+    firstDigit = true;
+    workingWithOp2 = false;
+    digitDisplay.textContent = "Can't Do That";
 }
 
 function roundNumbersWithLongDecimals(numberToRound) {
@@ -166,31 +178,63 @@ digitBtns.forEach((button) => {
 
 let operatorBtns = document.querySelectorAll(".operator");
 operatorBtns.forEach((button) => {
-    button.addEventListener('click', () => {
-        operand = button.id;
-        workingWithOp2 = true;
-        firstDigit = true;
-    });
+    button.addEventListener('click', () => changeOperator(button.id));
 });
 
 const clearBtn = document.getElementById("C");
-clearBtn.addEventListener('click', () => {
-    op1 = "0";
-    op2 = "0";
-    operand = "+";
-    workingWithOp2 = false;
-    firstDigit = true;
-    digitDisplay.textContent = "0";
-});
+clearBtn.addEventListener('click', () => clearAllData());
 
 const equalsBtn = document.getElementById("=");
-equalsBtn.addEventListener(('click'), () => calculateAndDisplayResult());
+equalsBtn.addEventListener('click', () => calculateAndDisplayResult());
 
 const backspaceBtn = document.getElementById("B");
-backspaceBtn.addEventListener(('click'), () => backSpaceDigit());
+backspaceBtn.addEventListener('click', () => backSpaceDigit());
 
 const switchSignBtn = document.getElementById("+/-");
-switchSignBtn.addEventListener(('click'), () => switchSign());
+switchSignBtn.addEventListener('click', () => switchSign());
 
 const dotBtn = document.getElementById(".");
-dotBtn.addEventListener(('click'), () => saveAndDisplayDot()); 
+dotBtn.addEventListener('click', () => saveAndDisplayDot()); 
+
+document.addEventListener('keydown', (e) =>  {
+    // isFinite checks for a finite number
+    if (isFinite(e.key)) {
+        saveAndDisplayDigit(e.key);
+    } else {
+        switch(e.key) {
+            case "c":
+            case "C":
+                clearAllData();
+                break;
+            case "b":
+            case "B":
+                backSpaceDigit();
+                break;
+            case "s":
+            case "S":
+                switchSign();
+                break;
+            case"/":
+                changeOperator("/");
+                break;
+            case "*":
+            case "x":
+            case "X":
+                changeOperator("*");
+                break;
+            case "-":
+                changeOperator("-");
+                break;
+            case "+":
+                changeOperator("+");
+                break;
+            case "=":
+            case "Enter":
+                calculateAndDisplayResult();
+                break;
+            case ".":
+                saveAndDisplayDot();
+                break;
+        }
+    }
+});
